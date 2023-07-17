594e0a6b6cd46ba3359c2168237aeff3e0aed88f
Hector Garcia Rosales <ghectormus@gmail.com>
2023-07-17 09:41:37 -0600

    solving exercise hamming

--- hamming/src/main/groovy/Hamming.groovy
+++ hamming/src/main/groovy/Hamming.groovy
@@ -1,7 +1,17 @@
 class Hamming {
 
     def distance(strand1, strand2) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+      if (strand1.isEmpty()){
+        return 0
+      } else{
+          def list1 = strand1.split("").toList()
+          def list2 = strand2.split("").toList()
+          def commons = list1.intersect(list2)
+          def both = list1.plus(list2)
+          both.removeElement(commons)
+      }
+
     }
+
 
 }
