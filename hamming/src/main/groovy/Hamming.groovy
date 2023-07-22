53aa9393def76e659e1075221d42ea001e8b732b
Hector Garcia Rosales <ghectormus@gmail.com>
2023-07-21 18:15:54 -0600

    Refact: methods

--- hamming/src/main/groovy/Hamming.groovy
+++ hamming/src/main/groovy/Hamming.groovy
@@ -1,17 +1,13 @@
 class Hamming {
 
     def distance(strand1, strand2) {
-      if (strand1.isEmpty() || strand1 == strand2){
-        return 0
-      } else{
-          def list1 = strand1.split("").toList()
-          def list2 = strand2.split("").toList()
-          def commons = list1.intersect(list2)
-          def both = list1.plus(list2)
-          both.removeElement(commons)
+      result = 0
+      for (int i = 0; i < strand1.length(); i++){
+        if (strand1[i] != strand2[i]){
+          result++
+        }
       }
-
+      return result
     }
-
 
 }
