d46d58c3130c0b6839c4558dc4cf7d336651b2ac
Hector Garcia Rosales <ghectormus@gmail.com>
2023-07-30 19:39:01 -0600

    minum neccesary, test passing

---
+++ matrix/src/main/groovy/Matrix.groovy
@@ -0,0 +1,15 @@
+class Matrix {
+
+    Matrix(String asString = 1) {
+        //throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+    int[] row(int rowNumber) {
+      [1]
+       // throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+    int[] column(int columnNumber) {
+        throw new UnsupportedOperationException('Method implementation is missing')
+    }
+}
